38a49151afdb3f1ccc2c77e2331970ddeb1ff6f1
PCJohn <prithvichakra@gmail.com>
2018-04-01 07:30:39 +0530

    Faster EWC

--- ewc.py
+++ ewc.py
@@ -29,77 +29,86 @@ class FullyConn():
         self.y = tf.placeholder(tf.float32,shape=[None,10])
         self.keep_prob = tf.placeholder(tf.float32)
         self.learning_rate = tf.placeholder(tf.float32)
-        self.ewc_lambda = tf.placeholder(tf.float32)
+        self.rem = tf.placeholder(tf.float32,shape=[6])
 
         d = 28*28*1
         out = tf.reshape(self.x,[-1,d])
         #out = tf.nn.dropout(out,0.8)
 
-        w1 = tf.get_variable('w1',shape=[d,400],initializer=tf.contrib.layers.xavier_initializer())
-        b1 = tf.get_variable('b1',shape=[400],initializer=tf.zeros_initializer())
+        H = 50
+        w1 = tf.get_variable('w1',shape=[d,H],initializer=tf.contrib.layers.xavier_initializer())
+        b1 = tf.get_variable('b1',shape=[H],initializer=tf.zeros_initializer())
         out = tf.nn.relu(tf.matmul(out,w1)+b1)
         out = tf.nn.dropout(out,self.keep_prob)
         
-        w2 = tf.get_variable('w2',shape=[400,400],initializer=tf.contrib.layers.xavier_initializer())
-        b2 = tf.get_variable('b2',shape=[400],initializer=tf.zeros_initializer())
+        w2 = tf.get_variable('w2',shape=[H,H],initializer=tf.contrib.layers.xavier_initializer())
+        b2 = tf.get_variable('b2',shape=[H],initializer=tf.zeros_initializer())
         out = tf.nn.relu(tf.matmul(out,w2)+b2)
         out = tf.nn.dropout(out,self.keep_prob)
         
-        w3 = tf.get_variable('w3',shape=[400,10],initializer=tf.contrib.layers.xavier_initializer())
+        w3 = tf.get_variable('w3',shape=[H,10],initializer=tf.contrib.layers.xavier_initializer())
         b3 = tf.get_variable('b3',shape=[10],initializer=tf.zeros_initializer())
         self.out = tf.matmul(out,w3)+b3
 
         self.params = [w1,b1,w2,b2,w3,b3]
-        #self.loss = tf.reduce_mean(tf.losses.softmax_cross_entropy(onehot_labels=self.y,logits=self.out))
         self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.y,logits=self.out))
         self.ewc_loss = tf.Variable(0,tf.float32) #tf.constant(0.0,shape=[])
-        #final_loss = 0.5*self.lmbda*self.ewc_loss+self.loss
         self.train_step = tf.train.AdamOptimizer(learning_rate=self.learning_rate,epsilon=self.eps).minimize(self.loss)
-        #self.train_step = tf.train.GradientDescentOptimizer(learning_rate=self.lr).minimize(self.loss)
-        self.fisher = None
         self.n_tasks = 0
+
+        self.fisher = [np.zeros(v.get_shape().as_list()) for v in self.params]
+        self.anchor = [np.zeros(v.get_shape().as_list()) for v in self.params]
+        self.fish_ph = [tf.placeholder(tf.float32,shape=v.get_shape().as_list()) for v in self.params]
+        self.anch_ph = [tf.placeholder(tf.float32,shape=v.get_shape().as_list()) for v in self.params]
+        #REM VEC here!
+        self.rem_vec = tf.placeholder(tf.float32,shape=(6,))
+        self.fisher = [np.zeros(v.get_shape().as_list()) for v in self.params]
 
         prob_list = tf.nn.softmax(self.out)
         class_index = tf.to_int32(tf.multinomial(tf.log(prob_list),1)[0][0])
         self.sample_loss = tf.log(prob_list[0,class_index])
         self.sample_grad = tf.gradients(self.sample_loss,self.params)
         
-    def augment_loss_ewc(self,x,sample_count=1000):
+    #Update Fisher and anchors on new task
+    def new_fish_anch(self,x,sample_count=1000):
         anchor_var_val = self.sess.run(self.params)
-        anchor = [np.array(v,copy=True) for v in anchor_var_val]
-        self.fisher = [np.zeros(v.get_shape().as_list()) for v in self.params]
-        
+        self.anchor = [np.array(v,copy=True) for v in anchor_var_val]
+        F = [np.zeros(v.get_shape().as_list()) for v in self.params]
         M = len(self.params)
         sample_ind = np.random.randint(0,x.shape[0],sample_count)
         for i in range(sample_count):
             sample = x[sample_ind[i]]
             grad = self.sess.run(self.sample_grad,feed_dict={self.x:np.array([sample]),self.keep_prob:1})
             for v in range(M):
-                self.fisher[v] += np.square(grad[v])
-        if self.n_tasks == 0:
-            self.ewc_loss = 0
-        #self.ewc_loss = 0
+                F[v] += np.square(grad[v])
         for v in range(M):
-            self.fisher[v] = self.fisher[v]/float(sample_count)
-            self.ewc_loss += 0.5*self.lmbda*tf.reduce_sum(tf.multiply(np.float32(self.fisher[v]),tf.square(self.params[v]-anchor[v])))
+            F[v] = np.float32(F[v])/float(sample_count)
+        for v in range(M):
+            #self.fisher[v] += np.float32(F[v])
+            self.fisher[v] += F[v]
+        #self.indiv_fisher.append(F)
+        self.n_tasks += 1
+    
+    def setup_ewc_loss(self):
+        M = len(self.params)
+        self.ewc_loss = tf.reduce_sum([0.5*
+                                       self.lmbda*
+                                       tf.reduce_sum(
+                                       tf.multiply(self.fish_ph[v],
+                                       tf.square(self.params[v]-self.anch_ph[v]))) 
+                                       for v in range(M)])
         final_loss = tf.add(self.ewc_loss,self.loss)
         self.train_step = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(final_loss)
-        #self.train_step = tf.train.GradientDescentOptimizer(learning_rate=self.lr).minimize(final_loss)
-        k = tf.variables_initializer(
-            [v for v in tf.global_variables() if v.name.split(':')[0] in set(self.sess.run(tf.report_uninitialized_variables()))
-            ])
+        k = tf.variables_initializer([v for v in tf.global_variables() if v.name.split(':')[0] in set(self.sess.run(tf.report_uninitialized_variables()))])
         self.sess.run(k)
 
-        del anchor
-        self.n_tasks += 1
-
-    def simple_hyperopt(self,x,y,vx,vy,use_ewc=False):
+    def simple_hyperopt(self,x,y,vx,vy,rem_vec,use_ewc=False):
         saver = tf.train.Saver()
         tmp_file = 'tmp.ckpt'
         saver.save(self.sess,tmp_file)
         ind = range(x.shape[0])
         best = {'bz':100,'lr':1e-2,'epch':600,'val':0,'loss':999}
-        lr_range = [1e-4,1e-2]
+        lr_range = [1e-4,1e-3]
         r = 40
         while r > 0:
             saver.restore(self.sess,tmp_file)
@@ -112,7 +121,7 @@ class FullyConn():
             if e < 0.5:
                 epch = best['epch']
             else:
-                epch_range = [1000,6000]
+                epch_range = [0,2000]
                 epch = int(epch_range[0]+(epch_range[1]-epch_range[0])*np.random.random())
             e = np.random.random()
             if e < 0.5:
@@ -122,7 +131,7 @@ class FullyConn():
             for _ in range(epch):
                 bi = np.random.choice(ind,bz)
                 _,loss,eloss = self.sess.run([self.train_step,self.loss,self.ewc_loss],feed_dict={self.x:x[bi],self.y:y[bi],
-                                                                   self.keep_prob:(1-self.dropout),self.learning_rate:lr})
+                                                                   self.keep_prob:(1-self.dropout),self.learning_rate:lr,self.rem:rem_vec})
                 if np.isnan(loss):
                     lr_range[1] = lr
                     break
@@ -149,7 +158,7 @@ class FullyConn():
         os.remove(tmp_file+'.index')
         os.remove(tmp_file+'.data-00000-of-00001')
 
-    def train(self,train,val=None,log=True,use_ewc=False):
+    def train(self,train,rem_vec,val=None,log=True,use_ewc=False):
         if self.sess == None:
             self.sess = start_sess()
             self.sess.run(tf.global_variables_initializer())
@@ -159,21 +168,29 @@ class FullyConn():
         ind = range(x.shape[0])
         if log == True:
             print 'Hyperparameter search...'
-        #self.simple_hyperopt(x,y,vx,vy,use_ewc=use_ewc)
+        #self.simple_hyperopt(x,y,vx,vy,rem_vec,use_ewc=use_ewc)
         if log == True:
             print 'Training...'
+        first_task = (self.n_tasks == 0)
         for epoch in range(self.epch):
             bi = np.random.choice(ind,self.bz) #batch index
-            _,loss,eloss,py = self.sess.run([self.train_step,self.loss,self.ewc_loss,self.out],feed_dict={self.x:x[bi],
-                                        self.y:y[bi],self.keep_prob:(1-self.dropout),self.learning_rate:self.lr})
+            feed_dict = {self.x:x[bi],self.y:y[bi],self.keep_prob:(1-self.dropout),self.learning_rate:self.lr}
+            feed_dict.update({self.rem:rem_vec})
+            if use_ewc & (not first_task):
+                for v in range(len(self.params)):
+                    feed_dict.update({self.fish_ph[v]:self.fisher[v]})
+                    feed_dict.update({self.anch_ph[v]:self.anchor[v]})
+            _,loss,eloss,py = self.sess.run([self.train_step,self.loss,self.ewc_loss,self.out],feed_dict=feed_dict)
             if epoch%100 == 0:
                 pvy = self.sess.run(self.out,feed_dict={self.x:vx,self.keep_prob:1})
                 if log == True:
                     print 'Epoch',str(epoch)+': \tloss '+str(loss)+'\tewc '+str(eloss)+' \tval acc '+str(acc(pvy,vy))
-        
+        self.epch += 150
         if use_ewc == True:
-            self.augment_loss_ewc(vx)
-        
+            if first_task:
+                self.setup_ewc_loss()
+            self.new_fish_anch(vx)
+
         #end of train
 
     def test(self,x,y=None):
@@ -182,41 +199,3 @@ class FullyConn():
             return py
         else:
             return (py,acc(py,y))
-
-"""#(ds,vds),(pds_list,pvds_list) = mnist_permute.load(permute=1)
-ds,vds = mnist.load()
-x,y = map(np.float32,zip(*ds))
-vx,vy = map(np.float32,zip(*vds))
-print x.shape,y.shape,'--',vx.shape,vy.shape
-
-#EXPERIMENT
-sess = start_sess()
-model = FullyConn(sess)
-print 'Trask 0: simple MNIST...'
-model.train((x,y),val=(vx,vy))
-_,vacc = model.test(vx,y=vy)
-print 'Test on task 0',str(vacc)
-model.augment_loss_ewc(x,lmbda=400)
-
-#Permutation tasks
-val_ds = [(vx,vy)]
-for i in range(6):
-    tds,tvds = tasks.permute(ds,vds)
-    tx,ty = map(np.float32,zip(*tds))
-    tvx,tvy = map(np.float32,zip(*tvds))
-    val_ds.append((tvx,tvy))
-
-    print 'Training on task '+str(i+1)+'...'
-    #model.epch += 100
-    model.train((tx,ty),val=(tvx,tvy))
-    for j,(ovx,ovy) in enumerate(val_ds):
-        _,vacc = model.test(ovx,y=ovy)
-        print 'Test on task '+str(j)+' '+str(vacc)
-    #lmbda -= 200
-    #lmbda = max(10,lmbda)
-    model.augment_loss_ewc(tx,lmbda=30)
-
-#TODO: Split tasks
-
-sess.close()
-"""
--- exp.py
+++ exp.py
@@ -2,20 +2,20 @@ import numpy as np
 import tensorflow as tf
 from matplotlib import pyplot as plt
 
-import ewc
+import ewc_fg
 import mnist
 import tasks
 
 def exp_ewc_all((x,y),(vx,vy),n_tasks,use_ewc=False,use_dropout=False):
     #sess = start_sess()
     plots = []
-    model = ewc.FullyConn()
+    model = ewc_fg.FullyConn()
     if use_dropout == False:
         model.dropout = 0
     else:
         model.dropout = 0.5
     print 'Trask 0: simple MNIST...'
-    model.train((x,y),val=(vx,vy),use_ewc=use_ewc)
+    model.train((x,y),np.ones(6),val=(vx,vy),use_ewc=use_ewc)
     _,vacc = model.test(vx,y=vy)
     print 'Test on task 0',str(vacc)
 
@@ -24,13 +24,22 @@ def exp_ewc_all((x,y),(vx,vy),n_tasks,use_ewc=False,use_dropout=False):
     plots.append([(0,vacc)])
     accum_val = (list(vx),list(vy))
     for i in range(n_tasks):
+        #if i == 2:
+        #    model.lmbda = 0
+        #else:
+        #    model.lmbda = 400
         tds,tvds = tasks.permute(ds,vds)
         tx,ty = map(np.float32,zip(*tds))
         tvx,tvy = map(np.float32,zip(*tvds))
         val_ds.append((tvx,tvy))
         plots.append([])
         print 'Training on task '+str(i+1)+'...'
-        model.train((tx,ty),val=(tvx,tvy),log=True,use_ewc=use_ewc)
+        if i == 3:
+            rem_vec = [1,1,1,1,1,1]
+        else:
+            rem_vec = [1,1,1,1,1,1]
+        #rem_vec = [1,1,1,1,1,1]
+        model.train((tx,ty),val=(tvx,tvy),rem_vec=rem_vec,log=True,use_ewc=use_ewc)
         for j,(ovx,ovy) in enumerate(val_ds):
             _,vacc = model.test(ovx,y=ovy)
             print 'Test on task '+str(j)+' '+str(vacc)
@@ -55,7 +64,7 @@ x,y = map(np.float32,zip(*ds))
 vx,vy = map(np.float32,zip(*vds))
 print x.shape,y.shape,'--',vx.shape,vy.shape
 
-num_tasks = 5 
+num_tasks = 35
 for use_ewc in [True,False]:
     use_dropout = True
     print '*****','Tasks:',num_tasks,'EWC:',use_ewc,'Dropout:',use_dropout,'*****'
